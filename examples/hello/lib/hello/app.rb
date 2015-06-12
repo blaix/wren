603775289ff6680d339d2eaa20e641207f6f5d18
Justin Blake <justin@hentzia.com>
2015-06-12 15:07:23 -0400

    Clean up status/header duplication

--- examples/hello/lib/hello/app.rb
+++ examples/hello/lib/hello/app.rb
@@ -3,17 +3,18 @@ require "hello/actions"
 module Hello
   class App
     def call(env)
-      if env["PATH_INFO"] == "/hello"
+      body = if env["PATH_INFO"] == "/hello"
         if env["REQUEST_METHOD"] == "GET"
-          return [200, {}, actions.say_hello.call]
+          actions.say_hello.call
         end
       elsif env["PATH_INFO"] == "/world"
         if env["REQUEST_METHOD"] == "DELETE"
-          return [200, {}, actions.say_goodbye.call]
+          actions.say_goodbye.call
         end
       end
 
-      [404, {}, ""]
+      status = body ? 200 : 404
+      [status, {}, [body]]
     end
 
     private
